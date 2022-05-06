256e06f16df799f94825505c4d13b4a53ace3678
hafiz-aa <mhafiz.aa@gmail.com>
2022-05-06 13:13:47 +0700

    commit on 'master'

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <Modal header="Sign up for Giveaway!" text="Grab you discount now!" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Modal from './components/Modal'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Modal },
+  data(){
+    return {
+      title: 'My First Vue App :)'
+    }
+  },
+  methods: {
+    handleClick(){
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+    }
   }
 }
 </script>
@@ -22,5 +30,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
